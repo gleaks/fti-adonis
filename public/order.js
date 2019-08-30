8d5fb7cc8bd5c4d1100fa3024c21846b416add5e
Gareth Leak <gleaks@gmail.com>
2019-08-30 15:40:44 -0600

    experimenting with nested forms

--- public/order.js
+++ public/order.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('#newOrderDate').datepicker();
+    $("#addButton").click(function(e){
+        e.preventDefault();
+        $('#addProduct').before($('#product').html());
+        $('.full-width:last').select2();
+    });
 });
