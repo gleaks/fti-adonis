9c37403be1e6b5b7d803fae9a2c47baaa7de857c
Gareth Leak <gleaks@gmail.com>
2019-09-04 14:44:11 -0600

    Complete nested forms (I think)

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -5,7 +5,9 @@ const Customer = use('App/Models/Customer')
 const Product = use('App/Models/Product')
 
 class OrderController {
-  async home({view}) {
+  async home({
+    view
+  }) {
     // Fetch orders
     const orders = await Order.query().with('customer').with('products').fetch();
 
@@ -14,7 +16,9 @@ class OrderController {
     })
   }
 
-  async new({view}) {
+  async new({
+    view
+  }) {
     const customers = await Customer.all();
     const products = await Product.all();
     return view.render('orders/new', {
@@ -23,28 +27,41 @@ class OrderController {
     });
   }
 
-  async create({ request, response, session, auth}) {
-        const order = request.all();
+  async create({
+    request,
+    response,
+    session,
+    auth
+  }) {
+    const order = request.all();
 
-        const posted = await auth.user.orders().create({
-            name: order.name,
-            customer_id: order.customer,
-            date: order.date,
-            status: order.status
-        });
-        const postedproducts = await posted.products().attach(order.products)
+    const posted = await auth.user.orders().create({
+      name: order.name,
+      customer_id: order.customer,
+      date: order.date,
+      status: order.status
+    });
+    const postedproducts = await posted.products().attach(order.products)
 
-        session.flash({ message: 'Your Work Order has been created!' });
-        return response.redirect('/');
+    session.flash({
+      message: 'Your Work Order has been created!'
+    });
+    return response.redirect('/');
   }
 
-  async delete({ response, session, params}) {
-        const order = await Order.find(params.id);
+  async delete({
+    response,
+    session,
+    params
+  }) {
+    const order = await Order.find(params.id);
 
-        await order.delete();
-        session.flash({ message: 'Your Work Order has been removed'});
-        return response.redirect('back');
-    }
+    await order.delete();
+    session.flash({
+      message: 'Your Work Order has been removed'
+    });
+    return response.redirect('back');
+  }
 }
 
 module.exports = OrderController
--- public/order.js
+++ public/order.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
     var num = 0;
+
+    // If a form has submitted with error the product list is brought back as DIV's
+    // with oldproduct as the class. Check if they exist, apply select2 to the inputs
+    // and increment the base counting number to the number of the last product
+    if ($('.oldproduct').length) {
+      $('.oldproduct').select2({
+        placeholder: 'Select a Product',
+        allowClear: true
+      });
+      num = $('.oldproduct:last').attr('name').split('[')[1].split(']')[0];
+    }
+
+    // Start the javascript datepicker plugin
     $('#newOrderDate').datepicker();
 
+    // Start select2 javascript plugin on the Customer input
     $('.select2').select2({
       placeholder: 'Select a Customer',
       allowClear: true
     });
 
+    // When form fails check to see if the Status input has a previously set
+    // value and apply it to the select2 input if it exists.
+    var value = $('.oldstatus').data('value');
+    if (value != '') {
+      $('#statusInput').val(value).trigger('change');
+    }
+
+    // Apply a jquery listener to dynamically added Delete buttons on each
+    // row to remove the entire row.
     $('#workOrderForm').on('click', '.deleteButton', function(e){
       e.preventDefault();
       $(this).closest('.row').remove();
     });
 
+    // When you go to add an additional product row it copies the HTML of a
+    // hidden input, changes its name with the base incrementing number and
+    // finally applies the select2 plugin to the input.
     $('#addButton').click(function(e){
         e.preventDefault();
         num++;
@@ -22,4 +48,5 @@ $(document).ready(function() {
           allowClear: true
         });
     });
+
 });
