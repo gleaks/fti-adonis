979f2019b836ec95800a28336c88e529183177d9
Gareth Leak <gleaks@gmail.com>
2019-09-10 14:09:22 -0600

    added order filter to datatable

--- public/app.js
+++ public/app.js
@@ -3,11 +3,20 @@ $(document).ready(function() {
     $('body').pgNotification({message: $('#flashmessage').text()}).show();
   };
 
-  $('#orderTable').DataTable({
+  var orderTable = $('#orderTable').DataTable({
     dom: "ltipr"
   });
 
   $('[name=orderTable_length]').addClass('cs-select');
 
   $('#orderTable_length').after('<div class="pull-right"><select id="orderTable_select" class="cs-select"><option value="All">All</option><option value="Quote">Quote</option><option value="Possible Order">Possible Order</option><option value="Likely Order">Likely Order</option><option value="Confirmed Order">Confirmed Order</option><option value="Shipped">Shipped</option></select></div>')
+
+  $('#orderTable_select').change(function() {
+    var search = this.value
+    if(search === 'All') {
+      search = ''
+    };
+    orderTable.columns(4).search(search).draw();
+  });
+
 });
