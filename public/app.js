233197015eb62254cb5d26e0e0dca674956e3be8
Gareth Leak <gleaks@gmail.com>
2019-09-13 13:52:07 -0600

    added search bar to all tables

--- public/app.js
+++ public/app.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
 
   // Start DataTables on all the tables, without a search bar (the dom option)
   var orderTable = $('#orderTable').DataTable({
-    dom: "ltipr"
+    dom: '<"row"<"col-md-4"l><"col-md-4 text-center"f><"#orderTable_selectDiv.col-md-4 text-right">tipr'
   });
   var customerTable = $('#customerTable').DataTable({
-    dom: "ltipr"
+    dom: '<"row"<"col-md-4"l><"col-md-4 text-center"f><"#customerTable_selectDiv.col-md-4 text-right">tipr'
   });
   var productTable = $('#productTable').DataTable({
-    dom: "ltipr"
+    dom: '<"row"<"col-md-4"l><"col-md-4 text-center"f><"#productTable_selectDiv.col-md-4 text-right">tipr'
   });
 
   // Apply styling to the DataTables created length dropdown
@@ -21,17 +21,14 @@ $(document).ready(function() {
   $('[name=productTable_length]').addClass('cs-select');
 
   // Create a dropdown that allows you to filter the Order & Product table by its Category name
-  $('#orderTable_length').after('<div class="pull-right">\
-                                  <select id="orderTable_select" class="cs-select">\
-                                    <option value="All">All</option><option value="Quote">Quote</option>\
-                                    <option value="Possible Order">Possible Order</option>\
-                                    <option value="Likely Order">Likely Order</option>\
-                                    <option value="Confirmed Order">Confirmed Order</option>\
-                                    <option value="Shipped">Shipped</option>\
-                                  </select>\
-                                </div>')
-  $('#productTable_length').after('<div class="pull-right">\
-                                    <select id="productTable_select" class="cs-select">\
+  $('#orderTable_selectDiv').html('<select id="orderTable_select" class="cs-select">\
+                                  <option value="All">All</option><option value="Quote">Quote</option>\
+                                  <option value="Possible Order">Possible Order</option>\
+                                  <option value="Likely Order">Likely Order</option>\
+                                  <option value="Confirmed Order">Confirmed Order</option>\
+                                  <option value="Shipped">Shipped</option>\
+                                </select>')
+  $('#productTable_selectDiv').html('<select id="productTable_select" class="cs-select">\
                                       <option value="All">All</option>\
                                       <option value="Base System">Base System</option>\
                                       <option value="DC Instrument">DC Instrument</option>\
@@ -39,8 +36,7 @@ $(document).ready(function() {
                                       <option value="IC Instrument">IC Instrument</option>\
                                       <option value="Accessory">Accessory</option>\
                                       <option value="Option">Option</option>\
-                                    </select>\
-                                  </div>')
+                                    </select>')
 
   // When the dropdown is changed apply the value of the dropdown as a filter to the DataTable
   $('#orderTable_select').change(function() {
