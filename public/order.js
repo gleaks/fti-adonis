3ff0be557067f31d5e8545dec02ac5c10f6761a7
Gareth Leak <gleaks@gmail.com>
2019-10-01 14:21:20 -0600

    all js flow logic for systems is done

--- public/order.js
+++ public/order.js
@@ -18,96 +18,89 @@ $(document).ready(function() {
       $(this).find('i').toggleClass('pg-arrow_minimize pg-arrow_right')
     })
 
+    var num = 0
+    var paneclass = 'active'
+    if($('.tab-pane:last').attr('id') != 'system-example') {
+      console.log('last is not system-example and the num is' + num)
+      num = 1 + Number($('.tab-pane:last').attr('id').split('-')[1])
+      paneclass = ''
+    }
+    $('.tab-pane:last').after($('#system-example').prop('outerHTML'))
+    console.log('the number is now' + num)
+    $('.tab-pane:last').attr('id', 'system-' + num).addClass(paneclass)
+    $('.system-dropdown:last').select2()
+
+    $('#workOrderForm').on('select2:selecting', '.system-dropdown', function(e) {
+      pane = $(e.target).closest('.tab-pane')
+      if($(this).val() == '') {
+        newpane = 'system-' + (parseInt(num) + 1)
+        pane.after($('#system-example').prop('outerHTML'))
+        $('.tab-pane:last').attr('id', newpane)
+        $('.nav-tabs li:last a').text(e.params.args.data.text)
+        $('.nav-tabs li:last').after('<li><a data-toggle="tab" href="#' + newpane + '"><i class="pg-plus_circle"></i> Add a System</a></li>')
+        $('.system-dropdown:last').select2()
+        $(e.target).attr('name', 'systems[system-' + num + ']')
+      } else {
+        link = pane.attr('id')
+        console.log(link)
+        $('.nav-tabs a[href="#' + link + '"]').text(e.params.args.data.text)
+      }
+      switch(e.params.args.data.text) {
+        case '2 Channel Testhead':
+        case 'Upgrade Only':
+          pane.find('.motherboards').show()
+          pane.find('.motherboarda-dropdown').val('').trigger('change').attr('name', 'systems[system-' + num + '][motherboarda]').select2()
+          pane.find('.motherboardb-dropdown').val('').trigger('change').attr('name', 'systems[system-' + num + '][motherboardb]').select2()
+          pane.find('.external').show()
+          pane.find('.external-dropdown').val('').trigger('change').attr('name', 'systems[system-' + num + '][externals]').select2()
+          pane.find('.external:not(:first)').remove()
+          pane.find('.module-dropdown').val('').trigger('change')
+          pane.find('#motherboardaCollapse').collapse('hide')
+          pane.find('.motherboardaCollapse').hide()
+          pane.find('#motherboardbCollapse').collapse('hide')
+          pane.find('.motherboardbCollapse').hide()
+          break
+        default:
+          console.log('Dropdown case did not match any selectors')
+          break
+      }
+    num++
+    })
+    $('#workOrderForm').on('select2:selecting', '.motherboarda-dropdown', function(e) {
+      pane = $(e.target).closest('.tab-pane')
+      thisnum = Number(pane.attr('id').split('-')[1])
+      modules = pane.find('.motherboardaModules')
+      pane.find('.motherboardaCollapse').show()
+      pane.find('#motherboardaCollapse').collapse('show')
+      modules.show()
+      modules.find('.module-dropdown').val('').trigger('change').attr('name', 'systems[system-' + thisnum + '][motherboarda][modules]').select2()
+    })
+
+    $('#workOrderForm').on('select2:selecting', '.motherboardb-dropdown', function(e) {
+      pane = $(e.target).closest('.tab-pane')
+      thisnum = Number(pane.attr('id').split('-')[1])
+      modules = pane.find('.motherboardbModules')
+      pane.find('.motherboardbCollapse').show()
+      pane.find('#motherboardbCollapse').collapse('show')
+      modules.show()
+      modules.find('.module-dropdown').val('').trigger('change').attr('name', 'systems[system-' + thisnum + '][motherboarda][modules]').select2()
+    })
+
+    $('#workOrderForm').on('click', '.addExternal', function(e) {
+      e.preventDefault()
+      pane = $(e.target).closest('.tab-pane')
+      thisnum = Number(pane.attr('id').split('-')[1])
+      pane.find('.external').after($('#system-example .external').prop('outerHTML'))
+      pane.find('.external:last').show()
+      pane.find('.addExternal:last').toggleClass('addExternal btn-info removeExternal btn-danger').html('<i class="pg-minus_circle"></i> DEL')
+      pane.find('.external-dropdown:last').attr('name', 'systems[system-' + thisnum + '][externals]').select2()
+    })
+
     $('#workOrderForm').on('click', '.removeExternal', function(e){
       e.preventDefault()
       $(this).closest('.row').remove()
     })
 
-    var num = 0
-    var paneclass = 'active'
-    if($('.tab-pane:last').attr('id') != 'system-example') {
-      num = 1 + Number($('.tab-pane:last').attr('id').split('-')[1])
-      paneclass = ''
-    }
-    $('.tab-pane:last').after($('#system-example').prop('outerHTML'))
-    $('.tab-pane:last').attr('id', 'system-' + num).addClass(paneclass)
-    $('.system-dropdown:last').select2()
-    $('#workOrderForm').on('select2:select', '.system-dropdown', function(e) {
-      num++
-      newpane = 'system-' + num
-      pane = $(e.target).closest('.tab-pane')
-      pane.after($('#system-example').prop('outerHTML'))
-      $('.tab-pane:last').attr('id', newpane)
-      $('.nav-tabs li:last a').text(e.params.data.text)
-      $('.nav-tabs li:last').after('<li><a data-toggle="tab" href="#' + newpane + '"><i class="pg-plus_circle"></i> Add a System</a></li>')
-      newselect = $('.system-dropdown:last').select2({
-        placeholder: 'System Type',
-        allowClear: false
-      })
-      $(e.target).attr('name', 'systems[system-' + e.params.data.id + ']')
-      switch(e.params.data.text) {
-        case '2 Channel Testhead':
-        case 'Upgrade Only':
-          pane.find('.motherboards').show()
-          // pane.find('#motherboardbCollapse').collapse()
-          mba = pane.find('.motherboarda-dropdown').select2({
-            placeholder: 'Motherboard Side A',
-            allowClear: true
-          })
-          mba.on('select2:select', function(a) {
-            $(a.target).attr('name', 'systems[system-' + e.params.data.id + '][motherboarda]')
-            modules = pane.find('.motherboardaModules')
-            pane.find('.motherboardaCollapse').show()
-            modules.show()
-            pane.find('#motherboardaCollapse').collapse('show')
-            modules.find('.module-dropdown').attr('name', 'systems[system-' + e.params.data.id + '][motherboarda][modules]').select2({
-              allowClear: true
-            })
-          })
-          mbb = pane.find('.motherboardb-dropdown').select2({
-            placeholder: 'Motherboard Side B',
-            allowClear: true
-          })
-          mbb.on('select2:select', function(b) {
-            $(b.target).attr('name', 'systems[system-' + e.params.data.id + '][motherboardb]')
-            modules = pane.find('.motherboardbModules')
-            pane.find('.motherboardbCollapse').show()
-            modules.show()
-            pane.find('#motherboardbCollapse').collapse('show')
-            modules.find('.module-dropdown').attr('name', 'systems[system-' + e.params.data.id + '][motherboardb][modules]').select2({
-              allowClear: true
-            })
-          })
-          pane.find('.external').show()
-          pane.find('.external-dropdown').attr('name', 'systems[system-' + e.params.data.id + '][externals]').select2({
-            placeholder: 'External Module',
-            allowClear: true
-          })
-          $('.addExternal').click(function(c){
-            c.preventDefault()
-            pane.find('.external').after($('#system-example .external').prop('outerHTML'))
-            pane.find('.external:last').show()
-            pane.find('.addExternal:last').removeClass('addExternal btn-info').addClass('removeExternal btn-danger').html('<i class="pg-minus_circle"></i> DEL')
-            pane.find('.external-dropdown:last').attr('name', 'systems[system-' + e.params.data.id + '][externals]').select2({
-              placeholder: 'External Module',
-              allowClear: false
-            })
-          })
-          break
-        case 'Spares Kit':
-          $('.motherboarda-dropdown:last').val('6').trigger('change').attr('name', 'systems[system-' + e.params.data.id + '][motherboarda]')
-          $('.amodules:last').show()
-          $('.amodules:last .module-dropdown').attr('name', 'systems[system-' + e.params.data.id + '][motherboarda][modules]')
-          $('.amodules:last .module-dropdown').select2({
-            placeholder: 'Select a Module',
-            allowClear: true
-          })
-          break
-        default:
-          console.log('something went wrong')
-          break
-      }
-    })
     $('#customerModalForm').submit(function(e){
       e.preventDefault()
       var form = $(this)
